f92c8f902acbf32bb24767c524bdb1661cfce321
Joseph S. Cha <josephshincha@gmail.com>
2020-01-21 05:35:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   #Return index of value, otherwise return nil
   count = 0
-  while 
+  while count < array.length do 
   array.index(value_to_find)
 end
 
