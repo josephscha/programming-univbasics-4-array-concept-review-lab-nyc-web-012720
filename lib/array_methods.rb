b168f3e8414948cc127709f3741c6ed9b2ed2215
Joseph S. Cha <josephshincha@gmail.com>
2020-01-21 22:05:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,7 +18,8 @@ end
 
 def find_max_value(array) #Check EVERY integer in array, return HIGHEST value
   max_value = 0
-  array.length.times do 
+  array.length.times do |index|
+    
   end 
 
 end
