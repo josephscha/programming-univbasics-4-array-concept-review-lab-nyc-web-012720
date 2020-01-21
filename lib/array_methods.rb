d4983fc9230b0406864f970e74ff0076a0fe851f
Joseph S. Cha <josephshincha@gmail.com>
2020-01-21 05:48:26 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,6 +3,7 @@ def find_element_index(array, value_to_find)
   if array.include?(value_to_find) == true 
     return array.index(value_to_find)
   else return nil 
+  end 
 end 
 
 def find_max_value(array)
