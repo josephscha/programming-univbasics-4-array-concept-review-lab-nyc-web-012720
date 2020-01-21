e253ebee93e9fa2c1486e8188dd6aa0c561ce4dc
Joseph S. Cha <josephshincha@gmail.com>
2020-01-21 05:34:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,7 @@
 def find_element_index(array, value_to_find)
   #Return index of value, otherwise return nil
-  array.index 
+  count = 0
+  array.index(value_to_find)
 end
 
 def find_max_value(array)
