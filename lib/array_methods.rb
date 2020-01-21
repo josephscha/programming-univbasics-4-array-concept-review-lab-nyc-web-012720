a4c5ddf6ace31caa2a068fe728a42c8183ce7e8e
Joseph S. Cha <josephshincha@gmail.com>
2020-01-21 05:50:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,9 +8,10 @@ end
 
 def find_max_value(array)
   #Check every integer in array, return HIGHEST value
-  
+  array.min
 end
 
 def find_min_value(array)
   #Check every integer in array, return LOWEST value
+  array.max
 end
