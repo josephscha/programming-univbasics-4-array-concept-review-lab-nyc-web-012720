a93b2d42e4534642eefc2d0f7be276bc88b68f9a
Joseph S. Cha <josephshincha@gmail.com>
2020-01-21 05:58:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,15 +6,15 @@ def find_element_index(array, value_to_find)
   end
 end
 
-def find_max_value(array) #Check every integer in array, return HIGHEST value
-  count = 0 
-  while count < array.length do
-    if array[count] = array.max
-      return array[count]
-      count += 1
-    end
-  end
-end
+# def find_max_value(array) 
+#   count = 0 
+#   while count < array.length do
+#     if array[count] = array.max
+#       return array[count]
+#       count += 1
+#     end
+#   end
+# end
 
 def find_min_value(array)
   #Check every integer in array, return LOWEST value
